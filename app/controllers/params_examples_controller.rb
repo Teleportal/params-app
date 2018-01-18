14164a2df37d81baa4973e246b6f99bace51e7aa
Teleportal <joshthegreenberg@gmail.com>
2018-01-17 21:38:03 -0600

    played around with parameters

---
+++ app/controllers/params_examples_controller.rb
@@ -0,0 +1,12 @@
+class ParamsExamplesController < ApplicationController
+  def query_params_method
+    input_message = params["my_message"]
+    input_message_2 = params[:my_message_2]
+    render json: {message: input_message, message_2: input_message_2}
+  end
+
+  def segment_params_method
+    input_message = params[:this_is_a_variable]
+    render json: {value: input_message}
+  end
+end
